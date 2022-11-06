c0b4ef3cfd0fcca5d16a41a1972a1e8af44acc3c
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-11-06 01:17:12 -0300

    Update visual.py
    Está com alguns bugs, quando corrige aparece alguns erros, etc. Pretendo dar continuidade em breve.

--- desafio_calculadora/visual.py
+++ desafio_calculadora/visual.py
@@ -1,1 +1,20 @@
 # Design do programa
+from PySimpleGUI import PySimpleGUI as sg
+# Layout
+sg.theme('Default1')
+layout = [
+    [sg.Text('Valor 1: '), sg.Input(key = 'v1')],
+    [sg.Text('Escolha uma operação matemática (+, -, *, /, p para potência): '), sg.Input(key = 'o')],
+    [sg.Text('Valor 2: '), sg.Input(key = 'v2')],
+    [sg.Button('Calcular')],
+]
+# Janela
+janela = sg.Window('Tela', layout)
+# Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Calcular':
+        if valores['o'] == '+':
+            print('O resultado é: ', 'v1' + 'v2', '\n')
